c883fc91bda3140e7d5b57dddc43b52c5868cfcb
pusp joshi <erpushparaj23@gmail.com>
2024-12-26 20:06:10 +0545

    Leet code Target sum program using php

---
+++ Target_sum.php
@@ -0,0 +1,45 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer
+     */
+    public function findTargetSumWays(array $nums, int $target): int
+    {
+        $sum = array_sum($nums);
+        
+        if (($sum + $target) % 2 !== 0 || $sum < abs($target)) {
+            return 0;
+        }
+        
+        $subsetSum = ($sum + $target) / 2;
+        
+        return $this->countSubsetsWithSum($nums, $subsetSum);
+    }
+
+    private function countSubsetsWithSum(array $nums, int $sum): int
+    {
+        $dp    = array_fill(0, $sum + 1, 0);
+        
+        $dp[0] = 1;
+        
+        foreach ($nums as $num) {
+            for ($j = $sum; $j >= $num; $j--) {
+                echo "<pre>"; print_r($dp); echo "</pre>";
+                $dp[$j] += $dp[$j - $num];
+            }
+        }
+        
+        return $dp[$sum];
+    }
+}
+
+$solution = new Solution();
+
+$res = $solution->findTargetSumWays([1],1);
+
+echo "<pre>"; print_r($res); exit;
+?>
